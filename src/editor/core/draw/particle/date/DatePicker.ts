c6c2019004a2e3f69d7cde3106f31bea88571d9b
tomz <tomzio@qq.com>
2025-02-28 11:24:54 +0800

    feat: 修改日期控件格式化

--- src/editor/core/draw/particle/date/DatePicker.ts
+++ src/editor/core/draw/particle/date/DatePicker.ts
@@ -541,15 +541,20 @@ export class DatePicker {
     }
   }
 
-  public formatDate(date: Date, format = 'yyyy-MM-dd hh:mm:ss'): string {
+  public formatDate(date: Date, format = 'YYYY-MM-DD HH:mm:ss'): string {
     let dateString = format
     const dateOption = {
+      'Y+': date.getFullYear().toString(),
       'y+': date.getFullYear().toString(),
       'M+': (date.getMonth() + 1).toString(),
+      'D+': date.getDate().toString(),
       'd+': date.getDate().toString(),
-      'h+': date.getHours().toString(),
+      'h+': date.getHours() % 12 == 0 ? '12' : (date.getHours() % 12).toString(),
+      'H+': date.getHours().toString(),
       'm+': date.getMinutes().toString(),
-      's+': date.getSeconds().toString()
+      's+': date.getSeconds().toString(),
+      'q+': Math.floor((date.getMonth() + 3) / 3).toString(),
+      S: date.getMilliseconds().toString(),
     }
     for (const k in dateOption) {
       const reg = new RegExp('(' + k + ')').exec(format)
